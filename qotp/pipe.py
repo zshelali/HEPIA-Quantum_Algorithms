117157c5ca0f8ccefd8d1120d8f3bd008adf3f9c
zshelali <ali.hajeri24@gmail.com>
2025-11-26 18:50:42 +0200

    refactor(pipe): change variable names and behavior

--- qotp/pipe.py
+++ qotp/pipe.py
@@ -1,3 +1,4 @@
+from numpy import full
 import quantum_tools as qt
 from qiskit import ClassicalRegister
 from rich.traceback import install
@@ -16,7 +17,7 @@ def adder_pipe(a, b):
     cl = Client()
 
     # convert and store server circuit to standard
-    qc_standard = qt.to_standard(sv.circuit)
+    sv.circuit = qt.to_standard(sv.circuit)
 
     # encrypt x and y
     cipher_x = cl.encrypt(a, sv)
@@ -32,26 +33,28 @@ def adder_pipe(a, b):
 
     # update client's keys
     cl.keys = deepcopy(merged_keys)
-    cl.update_key(qc_standard)
+    cl.update_key(sv.circuit)
+    sv.circuit.draw("mpl", filename="./images/temp_circuit.png")
     print(f"\nBefore update: {merged_keys}")
     print(f"After update: {cl.keys}")
 
     # add encrypted x,y states and the classical registers to the server circuit
     cl_reg = ClassicalRegister(cipher_y.circuit.num_qubits)
-    full_circuit = cipher_y.circuit ^ cipher_x.circuit
-    full_circuit.add_register(cl_reg)
-    full_circuit.append(
+    final_circuit = cipher_y.circuit ^ cipher_x.circuit
+    final_circuit.add_register(cl_reg)
+    final_circuit.append(
         sv.circuit,
-        [_ for _ in range(full_circuit.num_qubits)],
+        [_ for _ in range(final_circuit.num_qubits)],
         [_ for _ in range(sv.circuit.num_clbits)],
     )
+
     # measure end result
-    full_circuit.measure(
+    final_circuit.measure(
         [k for k in range(offset, cipher_y.circuit.num_qubits + offset)], [0, 1]
     )
 
     # fetch and decrypt measured result
-    result = qt.get_result(full_circuit)
+    result = qt.get_result(final_circuit)
     result_decrypted = cl.decrypt(result)
 
     print("Final result: ", result_decrypted)
